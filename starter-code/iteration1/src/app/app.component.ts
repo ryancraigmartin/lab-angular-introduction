453887c03f6d4e730c25dac8bd2f8b521e4010e7
Ryan Martin <rmart106@fiu.edu>
2018-05-14 17:26:16 -0400

    Finished iteration 1.

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Book Gallery:';
+  photoArray = [{src: 'https://images-na.ssl-images-amazon.com/images/I/51qwSaHjj%2BL._SX352_BO1,204,203,200_.jpg', text: 'Ego is the Enemy'},
+               {src: 'https://images-na.ssl-images-amazon.com/images/I/41Z06PJRlZL.jpg', text: '12 Rules for Life'}, 
+               {src: 'https://images-na.ssl-images-amazon.com/images/I/51xcOSxtYEL._SX352_BO1,204,203,200_.jpg', text: 'The Obstacle is the Way'}];
 }
